c1d87f06409041f6d6658d6e6b194316cd160700
Hlíðar Aron Sigurðsson <hlidar85@gmail.com>
2024-05-16 09:46:30 +0000

    Add change status  to do item using server action

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,15 @@ export default async function Home() {
     });
     revalidatePath("/");
   };
+  const statusChange = async (id: number, status: string) => {
+    "use server";
+    await prisma.toDo.update({
+      where: { id: id },
+      data: { statusId: status },
+    });
+    revalidatePath("/");
+  };
+
   const statuses = await prisma.status.findMany();
   const toDoItems = await prisma.toDo.findMany();
   return (
@@ -51,29 +60,34 @@ export default async function Home() {
                 <Table.Row key={toDo.id}>
                   <Table.Cell>{toDo.toDo}</Table.Cell>
                   <Table.Cell align="right">
-                    <Select.Root defaultValue={toDo.statusId}>
-                      <Select.Trigger />
-                      <Select.Content>
-                        <Select.Group>
-                          <Select.Label>Status</Select.Label>
-                          {statuses.map((status) => (
-                            <Select.Item
-                              key={status.status}
-                              value={status.status}
-                            >
-                              {status.DisplayNames}
+                    <form>
+                      <Select.Root
+                        defaultValue={toDo.statusId}
+                        onValueChange={statusChange.bind(null, toDo.id)}
+                      >
+                        <Select.Trigger />
+                        <Select.Content>
+                          <Select.Group>
+                            <Select.Label>Status</Select.Label>
+                            {statuses.map((status) => (
+                              <Select.Item
+                                key={status.status}
+                                value={status.status}
+                              >
+                                {status.DisplayNames}
+                              </Select.Item>
+                            ))}
+                          </Select.Group>
+                          <Select.Group>
+                            <Select.Label>action</Select.Label>
+                            <Select.Item value="edit">Edit</Select.Item>
+                            <Select.Item value="delete">
+                              <Text>Delete</Text>
                             </Select.Item>
-                          ))}
-                        </Select.Group>
-                        <Select.Group>
-                          <Select.Label>action</Select.Label>
-                          <Select.Item value="edit">Edit</Select.Item>
-                          <Select.Item value="delete">
-                            <Text>Delete</Text>
-                          </Select.Item>
-                        </Select.Group>
-                      </Select.Content>
-                    </Select.Root>
+                          </Select.Group>
+                        </Select.Content>
+                      </Select.Root>
+                    </form>
                   </Table.Cell>
                 </Table.Row>
               ))}
